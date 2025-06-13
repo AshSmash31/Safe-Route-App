a6e232598c781a777cfc6dd5711d075c4124412e
Akshat Nair <akshat.nair31@gmail.com>
2025-06-13 10:09:24 -0500

    import react statements, set up index for routing

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,9 +5,15 @@ import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { useRouter } from 'expo-router';
+import React from 'react';
 
 export default function HomeScreen() {
+  const router = useRouter();
+
   return (
+    <>
+    
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
@@ -52,6 +58,7 @@ export default function HomeScreen() {
         </ThemedText>
       </ThemedView>
     </ParallaxScrollView>
+    </>
   );
 }
 
